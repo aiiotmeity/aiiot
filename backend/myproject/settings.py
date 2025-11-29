d1056ef7b0528e5b425e291dad2b32786bc068e4
aiiot_asiet <aiiot@adishankara.ac.in>
2025-11-29 12:32:43 +0530

    media issue

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -147,6 +147,12 @@ ROOT_URLCONF = 'myproject.urls'
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_QUERYSTRING_AUTH = False
 
+AWS_DEFAULT_ACL = None
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
@@ -260,7 +266,3 @@ REST_FRAMEWORK = {
 AWS_QUERYSTRING_AUTH = False 
 
 # Optional: Set the file to be 'public-read' upon upload (for future uploads)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-    'ACL': 'public-read', 
-}
